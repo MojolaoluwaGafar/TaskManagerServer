eba18f9f9b503904af65d8df452c8890aae05ba6
Mojolaoluwa Gafar <Mojolaoluwa1212@gmail.com>
2025-11-25 07:35:36 +0100

    renamed

--- db.ts
+++ db.ts
@@ -4,11 +4,15 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const pool = new Pool({
-  user: process.env.DB_USER,
   host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
   port: Number(process.env.DB_PORT),
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
 });
+
+pool.connect()
+  .then(() => console.log("DB Connected ✅"))
+  .catch(err => console.error("DB Connection Error ❌", err));
 
 export default pool;
